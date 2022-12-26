7616226e0f08dc58b0fb52d05aca9abc4e0d1803
Essien Nathaniel <thaniessy@gmail.com>
2022-12-26 04:14:41 +0000

    Added logic to pull questions from the API

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,22 +8,32 @@ const Home = () => {
   const [questions, setQuestions] = useState([])
   const [isLoaded, setIsLoaded] = useState(false)
 
-   
+
   const getQuiz = () => {
-    fetch("https://opentdb.com/api.php?amount=2&category=18&type=multiple")
+    fetch("https://opentdb.com/api.php?amount=1&category=18&type=multiple")
       .then(res => res.json())
-     .then(data => setQuestions([data]))
+      .then(data => setQuestions([data]))
   }
   useEffect(() => {
-getQuiz()
+    getQuiz()
   }, [])
-
+  let result;
   function startQuiz() {
-    if(questions.length !== 0){
-        const questionsResult = questions[0].result
-    console.log(questionsResult)
-    }
+    if (questions.length !== 0) {
+      result = questions[0].results[0]
+
     // setIsLoaded(true)
+      console.log(result)
+    const { question } = result
+    const quizElem = result.map(ques => {
+      return <div>question</div>
+      console.log(question)
+    })
+    }
+
+    // console.log("The final log" + questions)
+
+
   }
 
 
@@ -35,17 +45,17 @@ getQuiz()
   return (
     <div className={styles.home}>
       {!isLoaded ? (
-       <div className={styles.blop}>
-        <img src={blob5} alt='blop' className={styles.blop1} />
-        <img src={blobs} alt='blop' className={styles.blop2} />
-      <div className={styles.body}>
-        <h1>Quizzical</h1>
-        <p>Test your knowledge on different Topics</p>
-        <button typeof="button" onClick={startQuiz}>Start quiz</button>
-      </div>
-      </div>
+        <div className={styles.blop}>
+          <img src={blob5} alt='blop' className={styles.blop1} />
+          <img src={blobs} alt='blop' className={styles.blop2} />
+          <div className={styles.body}>
+            <h1>Quizzical</h1>
+            <p>Test your knowledge on different Topics</p>
+            <button typeof="button" onClick={startQuiz}>Start quiz</button>
+          </div>
+        </div>
       ) : '' }
-     
+
     </div>
   )
 }
