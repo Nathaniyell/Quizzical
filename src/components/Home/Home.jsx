807a0870433648f9766c052287065ef3e52e9fc4
Essien Nathaniel <thaniessy@gmail.com>
2023-02-17 12:14:41 +0000

    Add quizPage component, provide updates to logic on homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,60 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import blob5 from '../assets/blob5.svg'
-import blobs from '../assets/blobs.svg'
-import styles from './home.module.css'
-
+import React, { useState, useEffect } from 'react';
+import blob5 from '../assets/blob5.svg';
+import blobs from '../assets/blobs.svg';
+import styles from './home.module.css';
+import QuizQuestions from '../QuizPage/Quizquestions';
 
 const Home = () => {
-  const [questions, setQuestions] = useState([])
-  const [isLoaded, setIsLoaded] = useState(false)
-
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [quizzes, setQuizzes] = useState([]);
 
   const getQuiz = () => {
     fetch("https://opentdb.com/api.php?amount=5&category=18&type=multiple")
       .then(res => res.json())
       .then(data => {
-        const result = data.results
-        result.map(item=>{
-          const {incorrect_answers, question, correct_answer} = item
-          console.log(question)
-        })
+        const result = data.results;
+        setQuizzes(result);
       })
-  }
+      .catch(error => {
+        console.log("Error fetching quizzes:", error);
+        setQuizzes([]);
+      });
+  };
+
   useEffect(() => {
-    getQuiz()
-  }, [])
+    if (isLoaded) {
+      getQuiz();
+    }
+  }, [isLoaded]);
 
   function startQuiz() {
-   getQuiz()
-    }
-
-    // console.log("The final log" + questions)
-
-
-  
-
-
-
-
-
-
+    setIsLoaded(true);
+  }
 
   return (
     <div className={styles.home}>
-      {!isLoaded ? (
+      {isLoaded ? (
+        <QuizQuestions quizQ={quizzes} />
+      ) : (
         <div className={styles.blop}>
           <img src={blob5} alt='blop' className={styles.blop1} />
           <img src={blobs} alt='blop' className={styles.blop2} />
           <div className={styles.body}>
             <h1>Quizzical</h1>
-            <p>Test your knowledge on different Topics</p>
-            <button typeof="button" onClick={startQuiz}>Start quiz</button>
+            <p>Test your knowledge on different topics</p>
+            <button type="button" onClick={startQuiz}>Start quiz</button>
           </div>
         </div>
-      ) : ''}
-
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+
